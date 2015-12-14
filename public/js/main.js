3266bfb5e1b79a02028689f786f69761101360bb
Lauren Davis <londond9218@gmail.com>
2015-12-14 16:09:55 -0700

    added and completed rates page

--- public/js/main.js
+++ public/js/main.js
@@ -46,6 +46,12 @@ angular.module('App')
 		$scope.mechanic = false;
 	}
 
+	$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
+
 }])
 
 
